88feb27b1ccdc5583f046c146cb4dabe0eeeab29
Saeed Dashti <saeed.da1994@gmail.com>
2025-09-13 22:10:09 +0330

    initial a react app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CourseList from "./components/CourseList/CourseList"
+
+function App(){
+  return(
+    <div>
+      <h1>courses (3)</h1>
+      <CourseList/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CourseList/CourseList.jsx
@@ -0,0 +1,38 @@
+import './CourseList.css'
+
+function CourseList(){
+    return(
+        <div className="course-list">
+        <div className="course-item">
+          <div className="course-item__image">
+            <img src="../../../public/img1.jpg" alt="Course" />
+          </div>
+          <div className="course-item__detail">
+            <div className="course-item__body">
+              <div>
+                <p className="title">React course</p>
+                <p className="desc">The Ultimate React.js and Redux Courses</p>
+              </div>
+              <span className="rate">4</span>
+            </div>
+            <div className="course-item__footer">
+              <div className="tags">
+                <span className="badge badge--secondary">react</span>
+                <span className="badge badge--secondary">front-end</span>
+              </div>
+              <div className="captions">
+                <div className="date">{new Date().toLocaleDateString('en-US', {
+                  month: 'short',
+                  year: 'numeric',
+                  day: 'numeric'
+                })}</div>
+                <span className="badge badge--primary">completed</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default CourseList
