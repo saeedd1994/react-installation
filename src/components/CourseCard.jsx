698d05880f39b16df08f45fcadeb4389a8455cc7
Saeed Dashti <saeed.da1994@gmail.com>
2025-09-19 22:20:37 +0330

    extracting components

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,30 +1,43 @@
-function CourseCard(props) {
-    console.log('props : ', props.course)
+function CourseCard({course}) {
     return(
         <div className="course-item">
           <div className="course-item__image">
-            <img src="../../public/img1.jpg" alt="Course" />
+            <img src={`../../public${course.imgUrl}`} alt="Course" />
           </div>
           <div className="course-item__detail">
-            <div className="course-item__body">
-              <div>
-                <p className="title">{props.course.title}</p>
-                <p className="desc">{props.course.desc}</p>
-              </div>
-              <span className="rate">{props.course.rate}</span>
-            </div>
-            <div className="course-item__footer">
-              <div className="tags">
-                  {props.course.tags.map(tag => (<span key={tag} className="badge badge--secondary">react</span>))}
-              </div>
-              <div className="captions">
-                <div className="date">{props.course.start}</div>
-                <span className="badge badge--primary">{props.course.status}</span>
-              </div>
-            </div>
+              <CourseCardBody course= {course}/>
+              <CourseCardFooter course= {course}/>
           </div>
         </div>
     )
 }
 
 export default CourseCard
+
+function CourseCardBody({course}){
+    return(
+        <div className="course-item__body">
+            <div>
+                <p className="title">{course.title}</p>
+                <p className="desc">{course.desc}</p>
+            </div>
+            <span className="rate">{course.rate}</span>
+        </div>
+
+    )
+}
+
+function CourseCardFooter({course}){
+    return(
+        <div className="course-item__footer">
+            <div className="tags">
+                {course.tags.map(tag => (<span key={tag} className="badge badge--secondary">react</span>))}
+            </div>
+            <div className="captions">
+                <div className="date">{course.start}</div>
+                <span className="badge badge--primary">{course.status}</span>
+            </div>
+        </div>
+
+    )
+}
